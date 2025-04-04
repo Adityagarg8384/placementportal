a29f3a5112da2d83f387753dab9ad7e8cc5da5fe
Aditya <adityagarg8384@gmail.com>
2025-04-04 20:10:22 +0530

    Adding files

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,7 +1,19 @@
 const mongoose=require("mongoose");
+require("dotenv").config();
+
+const PORTURL=process.env.PORTURL
 
 const dbconnect= ()=>{
-    mongoose.connect("mongodb://localhost:27017").then(()=>{
+    // console.log(PORTURL)
+    // mongoose.connect("mongodb://localhost:27017").then(()=>{
+    //     console.log("connected to databse");
+    // })
+    // .catch((err)=>{
+    //     console.log(err);
+    //     console.log("Some error occurred");
+    // })
+   
+    mongoose.connect(PORTURL).then(()=>{
         console.log("connected to databse");
     })
     .catch((err)=>{
